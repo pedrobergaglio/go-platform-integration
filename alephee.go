282a68de8eb6f3141f3325833aadd19860014737
Pedro Bergaglio <pedrobergaglio04@gmail.com>
2023-07-20 18:00:30 -0300

    starting alephee integration

---
+++ alephee.go
@@ -0,0 +1,12 @@
+package main
+
+func updateRumboPricesAlephee() {
+
+	//get products with alephee codes
+
+	//for each product, get the price and update it in the app
+	//if index in a multiple of 30, sleep 1 minute
+
+	//
+
+}
